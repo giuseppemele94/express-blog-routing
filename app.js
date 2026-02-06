ceaa9ef9f6987c78768dda65a842380cb4972467
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-06 15:00:44 +0100

    rotta home test funzionamento postman

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Ciao");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('public'));
+
+
+//rottta Home APP
+app.get('/', (req,res) => {
+    res.send("<h1>Rotta home</h1>")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
